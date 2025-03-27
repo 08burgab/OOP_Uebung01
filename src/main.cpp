8f8475f35bb5a87ee0cbb8f79b11432b6c64678a
Jonas Gasser <jonas.gasser15@gmail.com>
2025-03-27 12:01:37 +0100

    6. commit helper.h Jonas

--- src/main.cpp
+++ src/main.cpp
@@ -27,16 +27,15 @@
 #include "analogblinker.h"
 #include "helper.h"
 
-
 #define LED1 6    // Low-side switch (leuchtet mit LOW)
 #define LED2 3    // Low-side switch (leuchtet mit LOW)
 #define TASTER1 4 // LOW wenn gedrückt
 #define TASTER2 5 // LOW wenn gedrückt
-#define POT1 A7   
+#define POT1 A7
+#define DB_ZEIT 50
 
 blinker digitalblinker1;
 analogblinker analogblinker1;
-
 
 void setup()
 {
@@ -47,15 +46,26 @@ void setup()
     pinMode(TASTER1, INPUT_PULLUP);
     pinMode(TASTER2, INPUT_PULLUP);
     pinMode(POT1, INPUT);
+    db_taster1.init(TASTER1, DB_ZEIT, true);
+    db_taster2.init(TASTER2, DB_ZEIT, false);
+    fl_taster1.init(&db_taster1.out);
+    fl_taster2.init(&db_taster2.out);
 }
 
 void loop()
 {
-    digitalblinker1.poll();
+
     analogblinker1.poll();
     db_taster1.poll();
     db_taster2.poll();
     fl_taster1.poll();
     fl_taster2.poll();
 
-}
+    if (fl_taster1.pos)
+    {
+        digitalblinker1.poll();
+    }
+    if (fl_taster2.pos)
+    {
+        analogblinker1.poll();
+    }
