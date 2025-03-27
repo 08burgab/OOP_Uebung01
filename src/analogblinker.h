e1b8856c692cbc07f66e7a2b1a058f357ec9efc6
Gabriel Burger <08burgab@rgtfo-me.it>
2025-03-27 12:24:08 +0100

    ab change

--- src/analogblinker.h
+++ src/analogblinker.h
@@ -7,10 +7,9 @@
 class analogblinker
 {
 public:
-    bool sync = false, enable = false;
-    uint8_t step = 0;
+    bool enable = false;
     uint16_t blinkTime = 0;
-    void setblinktime(uint16_t);
+    
 
     // PinNr1, PinNr2, Blinkzeit[ms], Abfrage-Zeit[ms], Sync-Modus, Enable[T/F]
     void init(uint8_t, uint8_t, uint16_t, uint8_t, bool, bool);
@@ -20,6 +19,10 @@ public:
 private:
     unsigned long lastpoll = 0, polltime = 0;
     uint8_t pin1 = 0, pin2 = 0, dutycycle = 0;
+    uint8_t step = 0;
+    bool sync = false;
+    
+    void setblinktime(uint16_t);
 };
 
 
