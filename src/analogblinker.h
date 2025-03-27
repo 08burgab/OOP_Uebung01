70c1a6669b95ade561349348b8fc822d1a6f2ec6
Gabriel Burger <08burgab@rgtfo-me.it>
2025-03-27 08:25:15 +0100

    mein erstes Skibidi commit

--- src/analogblinker.h
+++ src/analogblinker.h
@@ -4,6 +4,27 @@
 
 #include <Arduino.h>
 
+class analogblinker
+{
+public:
+    bool sync = false, enable = false;
+    uint8_t step = 0;
+    uint16_t blinkTime = 0;
+    void setblinktime(uint16_t);
+
+    // PinNr1, PinNr2, Blinkzeit[ms], Abfrage-Zeit[ms], Sync-Modus, Enable[T/F]
+    void init(uint8_t, uint8_t, uint16_t, uint8_t, bool, bool);
+
+    void poll();
+
+private:
+    unsigned long lastpoll = 0, polltime = 0;
+    uint8_t pin1 = 0, pin2 = 0, dutycycle = 0;
+};
+
+
+
 
 
 #endif
+
